4a41e08bf98b7ed1815202ff43e9dc64a80f8476
Qingyang YE <112998980+YE-Qingyang@users.noreply.github.com>
2022-11-21 20:28:15 +0800

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -2,7 +2,6 @@ package application.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -14,274 +13,274 @@ import javafx.scene.shape.Rectangle;
 
 
 public class Controller implements Initializable {
-    private static final int PLAY_1 = 1;
-    private static final int PLAY_2 = 2;
-    private static final int EMPTY = 0;
-    private static final int BOUND = 90;
-    private static final int OFFSET = 15;
+  private static final int PLAY_1 = 1;
+  private static final int PLAY_2 = 2;
+  private static final int EMPTY = 0;
+  private static final int BOUND = 90;
+  private static final int OFFSET = 15;
 
-    @FXML
-    private Label ChessType;
+  @FXML
+  private Label ChessType;
 
-    @FXML
-    private Label linkStatus;
+  @FXML
+  private Label linkStatus;
 
-    @FXML
-    private Pane base_square;
+  @FXML
+  private Pane base_square;
 
-    @FXML
-    private Label gameResult;
+  @FXML
+  private Label gameResult;
 
-    public Rectangle getGame_panel() {
-        return game_panel;
+  public Rectangle getGame_panel() {
+    return game_panel;
+  }
+
+  @FXML
+  private Rectangle game_panel;
+
+  @FXML
+  private Label roomNumber;
+
+  @FXML
+  private Label turn;
+  private String position;
+
+  @FXML
+  private Label waitConnect;
+
+  private boolean TURN;
+
+  private static final int[][] chessBoard = new int[3][3];
+  private static final boolean[][] flag = new boolean[3][3];
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    game_panel.setOnMousePressed(event -> {
+      if (isYourTurn() && (!isFinish()) && isConnect()) {
+        int x = (int) (event.getX() / BOUND);
+        int y = (int) (event.getY() / BOUND);
+        position = x + "," + y;
+        refreshBoard(x, y);
+      }
+    });
+  }
+
+  public boolean isConnect() {
+    return waitConnect.getText().equals("Connected!");
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public void setResult(int player) {
+    if (isWin(player)) {
+      gameResult.setText("You win! Game over.");
+      clearTurnField();
     }
-
-    @FXML
-    private Rectangle game_panel;
-
-    @FXML
-    private Label roomNumber;
-
-    @FXML
-    private Label turn;
-    private String position;
-
-    @FXML
-    private Label waitConnect;
-
-    private boolean TURN;
-
-    private static final int[][] chessBoard = new int[3][3];
-    private static final boolean[][] flag = new boolean[3][3];
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        game_panel.setOnMousePressed(event -> {
-            if (isYourTurn() && (!isFinish()) && isConnect()) {
-                int x = (int) (event.getX() / BOUND);
-                int y = (int) (event.getY() / BOUND);
-                position = x + "," + y;
-                refreshBoard(x, y);
-            }
-        });
+    if (isWin(3 - player)) {
+      gameResult.setText("You lose... Game over.");
+      clearTurnField();
     }
-
-    public boolean isConnect() {
-        return waitConnect.getText().equals("Connected!");
+    if (isDraw()) {
+      gameResult.setText("The game is tied. Game over.");
+      clearTurnField();
     }
+  }
 
-    public String getPosition() {
-        return position;
+
+  public void setTurnField(boolean isYourTurn) {
+    if (!isFinish()) {
+      if (isYourTurn) {
+        turn.setText("It's your turn!");
+      } else {
+        turn.setText("Not your turn...");
+      }
     }
+  }
 
-    public void setResult(int player) {
-        if (isWin(player)) {
-            gameResult.setText("You win! Game over.");
-            clearTurnField();
+  public void clearTurnField() {
+    turn.setText("");
+  }
+
+  public boolean isYourTurn() {
+    return turn.getText().equals("It's your turn!");
+  }
+
+  public void setTURN(boolean isCircle) {
+    TURN = isCircle;
+  }
+
+  public void setWaitConnect(String text) {
+    waitConnect.setText(text);
+  }
+
+  public Label getRoomNumber() {
+    return roomNumber;
+  }
+
+  public void setLinkStatus(String text) {
+    linkStatus.setText(text);
+  }
+
+  private boolean refreshBoard(int x, int y) {
+    if (chessBoard[x][y] == EMPTY) {
+      chessBoard[x][y] = TURN ? PLAY_1 : PLAY_2;
+      drawChess();
+      return true;
+    }
+    return false;
+  }
+
+  public String getChessBoard() {
+    StringBuilder chess = new StringBuilder();
+    for (int i = 0; i < chessBoard.length; i++) {
+      for (int j = 0; j < chessBoard[0].length; j++) {
+        switch (chessBoard[i][j]) {
+          case EMPTY:
+            chess.append("0");
+            break;
+          case PLAY_1:
+            chess.append("1");
+            break;
+          case PLAY_2:
+            chess.append("2");
+            break;
+          default:
+            break;
         }
-        if (isWin(3 - player)) {
-            gameResult.setText("You lose... Game over.");
-            clearTurnField();
+      }
+    }
+    return chess.toString();
+  }
+
+  public void restoreChessBoard(String chessboard) {
+    int cnt = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        chessBoard[i][j] = Integer.parseInt(String.valueOf(chessboard.charAt(cnt)));
+        cnt++;
+      }
+    }
+    drawChess();
+  }
+
+  public void refresh(int x, int y) {
+    if (chessBoard[x][y] == EMPTY) {
+      chessBoard[x][y] = !TURN ? PLAY_1 : PLAY_2;
+      drawChess();
+    }
+  }
+
+  private void drawChess() {
+    for (int i = 0; i < chessBoard.length; i++) {
+      for (int j = 0; j < chessBoard[0].length; j++) {
+        if (flag[i][j]) {
+          // This square has been drawing, ignore.
+          continue;
         }
-        if (isDraw()) {
-            gameResult.setText("The game is tied. Game over.");
-            clearTurnField();
+        switch (chessBoard[i][j]) {
+          case PLAY_1:
+            drawCircle(i, j);
+            break;
+          case PLAY_2:
+            drawLine(i, j);
+            break;
+          case EMPTY:
+            // do nothing
+            break;
+          default:
+            System.err.println("Invalid value!");
         }
+      }
     }
+  }
 
+  private void drawCircle(int i, int j) {
+    Circle circle = new Circle();
+    base_square.getChildren().add(circle);
+    circle.setCenterX(i * BOUND + BOUND / 2.0 + OFFSET);
+    circle.setCenterY(j * BOUND + BOUND / 2.0 + OFFSET);
+    circle.setRadius(BOUND / 2.0 - OFFSET / 2.0);
+    circle.setStroke(Color.RED);
+    circle.setFill(Color.TRANSPARENT);
+    flag[i][j] = true;
+  }
 
-    public void setTurnField(boolean isYourTurn) {
-        if (!isFinish()) {
-            if (isYourTurn) {
-                turn.setText("It's your turn!");
-            } else {
-                turn.setText("Not your turn...");
-            }
+  private void drawLine(int i, int j) {
+    Line line_a = new Line();
+    Line line_b = new Line();
+    base_square.getChildren().add(line_a);
+    base_square.getChildren().add(line_b);
+    line_a.setStartX(i * BOUND + OFFSET * 1.5);
+    line_a.setStartY(j * BOUND + OFFSET * 1.5);
+    line_a.setEndX((i + 1) * BOUND + OFFSET * 0.5);
+    line_a.setEndY((j + 1) * BOUND + OFFSET * 0.5);
+    line_a.setStroke(Color.BLUE);
+
+    line_b.setStartX((i + 1) * BOUND + OFFSET * 0.5);
+    line_b.setStartY(j * BOUND + OFFSET * 1.5);
+    line_b.setEndX(i * BOUND + OFFSET * 1.5);
+    line_b.setEndY((j + 1) * BOUND + OFFSET * 0.5);
+    line_b.setStroke(Color.BLUE);
+    flag[i][j] = true;
+  }
+
+  public Label getChessType() {
+    return ChessType;
+  }
+
+  public boolean isWin(int player) {
+    for (int i = 0; i < chessBoard.length; i++) {
+      int num = 0;
+      for (int j = 0; j < chessBoard[0].length; j++) {
+        if (chessBoard[i][j] == player) {
+          num++;
         }
+      }
+      if (num == 3) {
+        return true;
+      }
+      num = 0;
     }
 
-    public void clearTurnField() {
-        turn.setText("");
-    }
-
-    public boolean isYourTurn() {
-        return turn.getText().equals("It's your turn!");
-    }
-
-    public void setTURN(boolean isCircle) {
-        TURN = isCircle;
-    }
-
-    public void setWaitConnect(String text) {
-        waitConnect.setText(text);
-    }
-
-    public Label getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setLinkStatus(String text) {
-        linkStatus.setText(text);
-    }
-
-    private boolean refreshBoard(int x, int y) {
-        if (chessBoard[x][y] == EMPTY) {
-            chessBoard[x][y] = TURN ? PLAY_1 : PLAY_2;
-            drawChess();
-            return true;
+    for (int j = 0; j < chessBoard.length; j++) {
+      int num = 0;
+      for (int i = 0; i < chessBoard[0].length; i++) {
+        if (chessBoard[i][j] == player) {
+          num++;
         }
-        return false;
+      }
+      if (num == 3) {
+        return true;
+      }
+      num = 0;
     }
 
-    public String getChessBoard() {
-        StringBuilder chess = new StringBuilder();
-        for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[0].length; j++) {
-                switch (chessBoard[i][j]) {
-                    case EMPTY:
-                        chess.append("0");
-                        break;
-                    case PLAY_1:
-                        chess.append("1");
-                        break;
-                    case PLAY_2:
-                        chess.append("2");
-                        break;
-                    default:
-                        break;
-                }
-            }
+    if (chessBoard[1][1] == player) {
+      if (chessBoard[0][0] == player && chessBoard[2][2] == player) {
+        return true;
+      }
+      if (chessBoard[0][2] == player && chessBoard[2][0] == player) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public boolean isDraw() {
+    for (int i = 0; i < chessBoard.length; i++) {
+      for (int j = 0; j < chessBoard[0].length; j++) {
+        if (chessBoard[i][j] == EMPTY) {
+          return false;
         }
-        return chess.toString();
+      }
     }
+    return !isWin(PLAY_1) && (!isWin(PLAY_2));
+  }
 
-    public void restoreChessBoard(String chessboard) {
-        int cnt = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                chessBoard[i][j] = Integer.parseInt(String.valueOf(chessboard.charAt(cnt)));
-                cnt++;
-            }
-        }
-        drawChess();
-    }
-
-    public void refresh(int x, int y) {
-        if (chessBoard[x][y] == EMPTY) {
-            chessBoard[x][y] = !TURN ? PLAY_1 : PLAY_2;
-            drawChess();
-        }
-    }
-
-    private void drawChess() {
-        for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[0].length; j++) {
-                if (flag[i][j]) {
-                    // This square has been drawing, ignore.
-                    continue;
-                }
-                switch (chessBoard[i][j]) {
-                    case PLAY_1:
-                        drawCircle(i, j);
-                        break;
-                    case PLAY_2:
-                        drawLine(i, j);
-                        break;
-                    case EMPTY:
-                        // do nothing
-                        break;
-                    default:
-                        System.err.println("Invalid value!");
-                }
-            }
-        }
-    }
-
-    private void drawCircle(int i, int j) {
-        Circle circle = new Circle();
-        base_square.getChildren().add(circle);
-        circle.setCenterX(i * BOUND + BOUND / 2.0 + OFFSET);
-        circle.setCenterY(j * BOUND + BOUND / 2.0 + OFFSET);
-        circle.setRadius(BOUND / 2.0 - OFFSET / 2.0);
-        circle.setStroke(Color.RED);
-        circle.setFill(Color.TRANSPARENT);
-        flag[i][j] = true;
-    }
-
-    private void drawLine(int i, int j) {
-        Line line_a = new Line();
-        Line line_b = new Line();
-        base_square.getChildren().add(line_a);
-        base_square.getChildren().add(line_b);
-        line_a.setStartX(i * BOUND + OFFSET * 1.5);
-        line_a.setStartY(j * BOUND + OFFSET * 1.5);
-        line_a.setEndX((i + 1) * BOUND + OFFSET * 0.5);
-        line_a.setEndY((j + 1) * BOUND + OFFSET * 0.5);
-        line_a.setStroke(Color.BLUE);
-
-        line_b.setStartX((i + 1) * BOUND + OFFSET * 0.5);
-        line_b.setStartY(j * BOUND + OFFSET * 1.5);
-        line_b.setEndX(i * BOUND + OFFSET * 1.5);
-        line_b.setEndY((j + 1) * BOUND + OFFSET * 0.5);
-        line_b.setStroke(Color.BLUE);
-        flag[i][j] = true;
-    }
-
-    public Label getChessType() {
-        return ChessType;
-    }
-
-    public boolean isWin(int player) {
-        for (int i = 0; i < chessBoard.length; i++) {
-            int num = 0;
-            for (int j = 0; j < chessBoard[0].length; j++) {
-                if (chessBoard[i][j] == player) {
-                    num++;
-                }
-            }
-            if (num == 3) {
-                return true;
-            }
-            num = 0;
-        }
-
-        for (int j = 0; j < chessBoard.length; j++) {
-            int num = 0;
-            for (int i = 0; i < chessBoard[0].length; i++) {
-                if (chessBoard[i][j] == player) {
-                    num++;
-                }
-            }
-            if (num == 3) {
-                return true;
-            }
-            num = 0;
-        }
-
-        if (chessBoard[1][1] == player) {
-            if (chessBoard[0][0] == player && chessBoard[2][2] == player) {
-                return true;
-            }
-            if (chessBoard[0][2] == player && chessBoard[2][0] == player) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public boolean isDraw() {
-        for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[0].length; j++) {
-                if (chessBoard[i][j] == EMPTY) {
-                    return false;
-                }
-            }
-        }
-        return !isWin(PLAY_1) && (!isWin(PLAY_2));
-    }
-
-    public boolean isFinish() {
-        return isDraw() || isWin(PLAY_2) || isWin(PLAY_1);
-    }
+  public boolean isFinish() {
+    return isDraw() || isWin(PLAY_2) || isWin(PLAY_1);
+  }
 }
--- GameServer.java
+++ GameServer.java
@@ -9,202 +9,202 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class GameServer {
-    private int port = 8888;
-    private ServerSocket serverSocket;
-    private List<Player> clients;
-    private List<Match> matches;
-    private Lock lock;
+  private int port = 8888;
+  private ServerSocket serverSocket;
+  private List<Player> clients;
+  private List<Match> matches;
+  private Lock lock;
 
-    public GameServer() {
+  public GameServer() {
+    try {
+      serverSocket = new ServerSocket(this.port);
+    } catch (IOException e) {
+      // e.printStackTrace();
+    }
+    clients = new LinkedList<>();
+    lock = new ReentrantLock();
+    matches = new LinkedList<>();
+
+    while (true) {
+      try {
+        Socket clientSocket = serverSocket.accept();
+        System.out.println(clientSocket.getPort());
+        Player player = new Player(clientSocket);
+        clients.add(player);
+        int flag = 0;
+        Match match = null;
+        for (Match m : matches) {
+          if (!m.isConnected()) {
+            match = m;
+            flag = 1;
+            break;
+          }
+        }
+        if (flag == 0) {
+          match = new Match();
+          matches.add(match);
+        }
+        match.addPlayer(player);
+        player.send(player.getRoomNum(), "Link", "Success");
+        player.start();
+        if (flag == 0) {
+          match.start();
+        }
+      } catch (IOException e) {
+      // e.printStackTrace();
+      }
+    }
+  }
+
+  // store the data of each player and communicate message with clients
+  class Player extends Thread {
+    private Socket socket;
+    private boolean isMatched;
+    private ObjectOutputStream os;
+    private ObjectInputStream is;
+    private String roomNum;
+    private int playerType;
+
+    public Player(Socket socket) {
+      this.socket = socket;
+      isMatched = false;
+      roomNum = null;
+      try {
+        os = new ObjectOutputStream(socket.getOutputStream());
+        is = new ObjectInputStream(socket.getInputStream());
+      } catch (IOException e) {
+        // e.printStackTrace();
+      }
+    }
+
+    public void run() {
+      while (true) {
+        String s = null;
         try {
-            serverSocket = new ServerSocket(this.port);
-        } catch (IOException e) {
-            // e.printStackTrace();
-        }
-        clients = new LinkedList<>();
-        lock = new ReentrantLock();
-        matches = new LinkedList<>();
-
-        while (true) {
-            try {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println(clientSocket.getPort());
-                Player player = new Player(clientSocket);
-                clients.add(player);
-                int flag = 0;
-                Match match = null;
-                for (Match m : matches) {
-                    if (!m.isConnected()) {
-                        match = m;
-                        flag = 1;
-                        break;
-                    }
-                }
-                if (flag == 0) {
-                    match = new Match();
-                    matches.add(match);
-                }
-                match.addPlayer(player);
-                player.send(player.getRoomNum(), "Link", "Success");
-                player.start();
-                if (flag == 0) {
-                    match.start();
-                }
-            } catch (IOException e) {
-//                 e.printStackTrace();
+          s = (String) is.readObject();
+          System.out.println(s);
+        } catch (IOException | ClassNotFoundException e) {
+          // e.printStackTrace();
+          clients.forEach(p -> p.send(roomNum, "Quit", Integer.toString(this.getPlayerType())));
+          matches.forEach(m -> {
+            if (Integer.toString(m.room).equals(roomNum)) {
+              m.quitMatch(this.getPlayerType());
             }
+          });
+          break;
         }
+        if (s != null) {
+          // handle the message from clients
+          String[] message = s.split(":");
+          String room = message[0];
+          String key = message[1];
+          String value = message[2];
+
+          clients.forEach(p -> p.send(room, key, value));
+        }
+      }
     }
 
-    // store the data of each player and communicate message with clients
-    class Player extends Thread {
-        private Socket socket;
-        private boolean isMatched;
-        private ObjectOutputStream os;
-        private ObjectInputStream is;
-        private String roomNum;
-        private int playerType;
-
-        public Player(Socket socket) {
-            this.socket = socket;
-            isMatched = false;
-            roomNum = null;
-            try {
-                os = new ObjectOutputStream(socket.getOutputStream());
-                is = new ObjectInputStream(socket.getInputStream());
-            } catch (IOException e) {
-                // e.printStackTrace();
-            }
-        }
-
-        public void run() {
-            while (true) {
-                String s = null;
-                try {
-                    s = (String) is.readObject();
-                    System.out.println(s);
-                } catch (IOException | ClassNotFoundException e) {
-//                  e.printStackTrace();
-                    clients.forEach(p -> p.send(roomNum, "Quit", Integer.toString(this.getPlayerType())));
-                    matches.forEach(m -> {
-                        if (Integer.toString(m.room).equals(roomNum)) {
-                            m.quitMatch(this.getPlayerType());
-                        }
-                    });
-                    break;
-                }
-                if (s != null) {
-                    // handle the message from clients
-                    String[] message = s.split(":");
-                    String room = message[0];
-                    String key = message[1];
-                    String value = message[2];
-
-                    clients.forEach(p -> p.send(room, key, value));
-                }
-            }
-        }
-
-        public void send(String room, String type, String message) {
-            try {
-                os.writeObject(room + ":" + type + ":" + message);
-                os.flush();
-            } catch (IOException e) {
-                // e.printStackTrace();
-            }
-        }
-
-        public boolean isMatched() {
-            return isMatched;
-        }
-
-        public void setMatched(boolean matched) {
-            isMatched = matched;
-        }
-
-        public String getRoomNum() {
-            return roomNum;
-        }
-
-        public void setRoomNum(String roomNum) {
-            this.roomNum = roomNum;
-        }
-
-        public int getPlayerType() {
-            return playerType;
-        }
-
-        public void setPlayerType(int playerType) {
-            this.playerType = playerType;
-        }
+    public void send(String room, String type, String message) {
+      try {
+        os.writeObject(room + ":" + type + ":" + message);
+        os.flush();
+      } catch (IOException e) {
+        // e.printStackTrace();
+      }
     }
 
-
-    // match two player in a match
-    class Match extends Thread {
-        private Player circle;
-        private Player line;
-        private int room;
-
-        public Match() {
-            this.circle = null;
-            this.line = null;
-            room = (int) (Math.random() * 100 + 1);
-            for (Match m : matches) {
-                while (m.room == room) {
-                    room = (int) (Math.random() * 100 + 1);
-                }
-            }
-        }
-
-        public boolean isConnected() {
-            return circle != null && line != null;
-        }
-
-        public void addPlayer(Player player) {
-            if (circle == null) {
-                circle = player;
-                player.setRoomNum(Integer.toString(room));
-                player.setPlayerType(1);
-                player.send(player.getRoomNum(), "Chess", "Circle");
-                player.send(player.getRoomNum(), "Connect", "Waiting");
-            } else {
-                line = player;
-                player.setRoomNum(Integer.toString(room));
-                player.setPlayerType(2);
-                player.send(player.getRoomNum(), "Chess", "Line");
-                player.send(player.getRoomNum(), "Connect", "Waiting");
-            }
-            if (isConnected()) {
-                circle.send(player.getRoomNum(), "Connect", "Success");
-                line.send(player.getRoomNum(), "Connect", "Success");
-                circle.setMatched(true);
-                line.setMatched(true);
-            }
-        }
-
-        public void quitMatch(int type) {
-            if (type == 1) {
-                circle = null;
-                // line.send(line.getRoomNum(), "Connect", "Waiting");
-                if (line != null) {
-                    line.setMatched(false);
-                }
-            }
-            if (type == 2) {
-                line = null;
-                // circle.send(circle.getRoomNum(), "Connect", "Waiting");
-                if (circle != null) {
-                    circle.setMatched(false);
-                }
-            }
-
-        }
+    public boolean isMatched() {
+      return isMatched;
     }
 
-
-    public static void main(String[] args) {
-        System.out.println("Server is waiting for accept...");
-        GameServer gameServer = new GameServer();
+    public void setMatched(boolean matched) {
+      isMatched = matched;
     }
+
+    public String getRoomNum() {
+      return roomNum;
+    }
+
+    public void setRoomNum(String roomNum) {
+      this.roomNum = roomNum;
+    }
+
+    public int getPlayerType() {
+      return playerType;
+    }
+
+    public void setPlayerType(int playerType) {
+      this.playerType = playerType;
+    }
+  }
+
+
+  // match two player in a match
+  class Match extends Thread {
+    private Player circle;
+    private Player line;
+    private int room;
+
+    public Match() {
+      this.circle = null;
+      this.line = null;
+      room = (int) (Math.random() * 100 + 1);
+      for (Match m : matches) {
+        while (m.room == room) {
+          room = (int) (Math.random() * 100 + 1);
+        }
+      }
+    }
+
+    public boolean isConnected() {
+      return circle != null && line != null;
+    }
+
+    public void addPlayer(Player player) {
+      if (circle == null) {
+        circle = player;
+        player.setRoomNum(Integer.toString(room));
+        player.setPlayerType(1);
+        player.send(player.getRoomNum(), "Chess", "Circle");
+        player.send(player.getRoomNum(), "Connect", "Waiting");
+      } else {
+        line = player;
+        player.setRoomNum(Integer.toString(room));
+        player.setPlayerType(2);
+        player.send(player.getRoomNum(), "Chess", "Line");
+        player.send(player.getRoomNum(), "Connect", "Waiting");
+      }
+      if (isConnected()) {
+        circle.send(player.getRoomNum(), "Connect", "Success");
+        line.send(player.getRoomNum(), "Connect", "Success");
+        circle.setMatched(true);
+        line.setMatched(true);
+      }
+    }
+
+    public void quitMatch(int type) {
+      if (type == 1) {
+        circle = null;
+        // line.send(line.getRoomNum(), "Connect", "Waiting");
+        if (line != null) {
+          line.setMatched(false);
+        }
+      }
+      if (type == 2) {
+        line = null;
+        // circle.send(circle.getRoomNum(), "Connect", "Waiting");
+        if (circle != null) {
+          circle.setMatched(false);
+        }
+      }
+
+    }
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println("Server is waiting for accept...");
+    GameServer gameServer = new GameServer();
+  }
 }
